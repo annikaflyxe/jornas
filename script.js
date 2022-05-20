3d9380078e68008b3e502827adc7d920d9a1b1b8
Annika Flyxe <aflyxe@gmail.com>
2022-05-20 14:34:56 +0200

    nav bar and hero

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navmenu");
+const navLink = document.querySelectorAll(".navlink");
+const icons = document.querySelectorAll(".icons");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
